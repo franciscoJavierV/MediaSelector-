e527c13ca3fb3ab5793e6d76f4f0439a0bfdf2f7
Francisco Vasquez <franjavi4111@gmail.com>
2020-09-11 17:52:03 -0500

    done

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,55 +1,12 @@
 import React from 'react';
 import './styles/Home.css';
-
-addEventListener('DOMContentLoaded', () => {
-    const images = ['assets/new1.jpg', 'assets/new2.jpg', 'assets/new3.jpg', 'assets/new4.jpg']
-
-    let i = 1
-    const img1 = document.querySelector('#img1')
-    const img2 = document.querySelector('#img2')
-    const progress_bar = document.querySelector('#progress-bar')
-    const div_indicadores = document.querySelector('#indicadores')
-    let porcentaje_base = 100 / images.length
-    let porcentaje_actual = porcentaje_base
-
-    for (let index = 0; index < images.length; index++) {
-        const div = document.createElement('div')
-        div.classList.add('circles')
-        div.id = index
-        div_indicadores.appendChild(div)
-    }
-
-    progress_bar.style.width = `${porcentaje_base}%`
-    img1.src = images[0]
-    const circulos = document.querySelectorAll('.circles')
-    circulos[0].classList.add('resaltado')
-
-    const slideshow = () => {
-        img2.src = images[i]
-        const circulo_actual = Array.from(circulos).find(el => el.id == i)
-        Array.from(circulos).forEach(cir => cir.classList.remove('resaltado'))
-        circulo_actual.classList.add('resaltado')
-    
-        img2.classList.add('activeimg')
-        i++
-        porcentaje_actual += porcentaje_base
-        progress_bar.style.width = `${porcentaje_actual}%`
-
-        if (i == images - length) {
-            i = 0
-            porcentaje_actual = porcentaje_base - porcentaje_base
-        }
-
-        setTimeout(() => {
-            img1.src = img2.src
-            img2.classList.remove('activeimg')
-        }, 1000);
-    }
-    setInterval(slideshow, 4000)
-})
-
+import fractured from './assets/fractured.jpg';
+import umbrell from './assets/umbrella.jpg';
+import laotra from './assets/split.jpg';   
 function Home() {
     return (
+        <>
+        
         <div className="slides">
             <h1>
                 Novedades
@@ -76,25 +33,26 @@ function Home() {
             </h1>
             <div className="image">
                 <a target="_blank" href="assets/umbrella.jpg">
-                    <img src="assets/umbrella.jpg" alt="The Umbrella Academy" width="600px" height="400px"></img>
+                    <img src={umbrell} alt="The Umbrella Academy" width="600px" height="400px"></img>
                 </a>
                 <div class="desc">The Umbrella Academy</div>
             </div>
 
             <div className="image">
                 <a target="_blank" href="assets/fractured.jpg">
-                    <img src="assets/fractured.jpg" alt="Fractured" width="600" height="400"></img>
+                    <img src={fractured} alt="Fractured" width="600" height="400"></img>
                 </a>
                 <div className="desc">Fractured</div>
             </div>
 
             <div className="image">
                 <a target="_blank" href="assets/split.jpg">
-                    <img src="assets/spli.jpg" alt="Split" width="600" height="400"></img>
+                    <img src={laotra} alt="Split" width="600" height="400"></img>
                 </a>
                 <div className="desc">Split</div>
             </div>
         </section>
+        </>
     )
 }
-export default Home
+export default Home;
--- src/containers/Likes.jsx
+++ src/containers/Likes.jsx
@@ -12,6 +12,69 @@ function Likes() {
       <h1 className="likes-tittle">Conozcamos tus gustos...</h1>
       <form className="likes__form" onSubmit={handleSubmit}>
         <div className="likes__form-data">
+            <div className="likes__for,-data-group2"> 
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="romance"
+                value="comedia"
+                type="checkbox"
+              />
+              Misterio
+            </label>
+            <br></br>
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="romance"
+                value="comedia"
+                type="checkbox"
+              />
+              Telenovelas
+            </label>
+            <br></br>
+
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="comedia"
+                value="terror"
+                type="checkbox"
+              />
+              Animes
+            </label>
+            <br></br>
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="romance"
+                value="comedia"
+                type="checkbox"
+              />
+              Fantasia
+            </label>
+            <br></br>
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="comedia"
+                value="terror"
+                type="checkbox"
+              />
+              Documentales
+            </label>
+            <br></br>
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="romance"
+                value="comedia"
+                type="checkbox"
+              />
+              Policiales
+            </label>
+            
+            </div>
           <div className="likes__form-data-group1">
             <label className="likes__column-label">
               <input
@@ -66,71 +129,11 @@ function Likes() {
             <label className="likes__column-label">
               <input
                 className="likes__column__checkbox"
-                name="romance"
-                value="comedia"
-                type="checkbox"
-              />
-              Telenovelas
-            </label>
-            <br></br>
-            <label className="likes__column-label">
-              <input
-                className="likes__column__checkbox"
                 name="comedia"
                 value="terror"
                 type="checkbox"
               />
               Dramas
-            </label>
-            <br></br>
-            <label className="likes__column-label">
-              <input
-                className="likes__column__checkbox"
-                name="romance"
-                value="comedia"
-                type="checkbox"
-              />
-              Misterio
-            </label>
-            <br></br>
-            <label className="likes__column-label">
-              <input
-                className="likes__column__checkbox"
-                name="comedia"
-                value="terror"
-                type="checkbox"
-              />
-              Animes
-            </label>
-            <br></br>
-            <label className="likes__column-label">
-              <input
-                className="likes__column__checkbox"
-                name="romance"
-                value="comedia"
-                type="checkbox"
-              />
-              Fantasia
-            </label>
-            <br></br>
-            <label className="likes__column-label">
-              <input
-                className="likes__column__checkbox"
-                name="comedia"
-                value="terror"
-                type="checkbox"
-              />
-              Documentales
-            </label>
-            <br></br>
-            <label className="likes__column-label">
-              <input
-                className="likes__column__checkbox"
-                name="romance"
-                value="comedia"
-                type="checkbox"
-              />
-              Policiales
             </label>
             <br></br>
         <button type="submit"  className="likes__button">
