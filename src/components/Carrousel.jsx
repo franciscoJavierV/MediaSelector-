a35af8a1201799015fb32bafe58d3d7a80315e61
Francisco Vasquez <franjavi4111@gmail.com>
2020-09-11 18:27:59 -0500

    loca

---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/Carrousel.css';
+function Carrousel() {
+    return (
+        <div>
+            <h1>soy carrousel</h1>
+        </div>
+    )
+}
+
+export default Carrousel
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,56 +1,49 @@
-import React from 'react';
-import './styles/Home.css';
-import fractured from './assets/fractured.jpg';
-import umbrell from './assets/umbrella.jpg';
-import laotra from './assets/split.jpg';   
+import React from "react";
+import "./styles/Home.css";
+import Carrousel from "../components/Carrousel";
+import fractured from "./assets/fractured.jpg";
+import umbrell from "./assets/umbrella.jpg";
+import laotra from "./assets/split.jpg";
 
 function Home() {
-    return (
-        <div className="done">
-        <div className="slides">
-            
-            <div className="images">
-                <div id="progress-bar-container">
-                    <div id="progress-bar"></div>
-                </div>
-                    
-                <div id="slideshow">
-                    <img src="" alt="" id="img1"></img>
-                    <img src="" alt="" id="img2"></img>
-                </div>
-                    
-                <div id="indicadores">
-                    
-                </div>
-            </div>
-        </div>
-
+  return (
+    <>
+      {/*aca se renderiza el componente de carrousel */}
+      <Carrousel />
+      <div className="done">
         <section className="gallery">
-            <h1>
-                Te recomendamos...
-            </h1>
-            <div className="image">
-                <a target="_blank" href="assets/umbrella.jpg">
-                    <img src={umbrell} alt="The Umbrella Academy" width="600px" height="400px"></img>
-                </a>
-                <div className="desc">The Umbrella Academy</div>
-            </div>
-
-            <div className="image">
-                <a target="_blank" href="assets/fractured.jpg">
-                    <img src={fractured} alt="Fractured" width="600" height="400"></img>
-                </a>
-                <div className="desc">Fractured</div>
-            </div>
-
-            <div className="image">
-                <a target="_blank" href="assets/split.jpg">
-                    <img src={laotra} alt="Split" width="600" height="400"></img>
-                </a>
-                <div className="desc">Split</div>
-            </div>
+          <h1>Te recomendamos...</h1>
+          <div className="image">
+            <a target="_blank" href="assets/umbrella.jpg">
+              <img
+                src={umbrell}
+                alt="The Umbrella Academy"
+                width="600px"
+                height="400px"
+              ></img>
+            </a>
+            <div className="desc">The Umbrella Academy</div>
+          </div>
+          <div className="image">
+            <a target="_blank" href="assets/fractured.jpg">
+              <img
+                src={fractured}
+                alt="Fractured"
+                width="600"
+                height="400"
+              ></img>
+            </a>
+            <div className="desc">Fractured</div>
+          </div>
+          <div className="image">
+            <a target="_blank" href="assets/split.jpg">
+              <img src={laotra} alt="Split" width="600" height="400"></img>
+            </a>
+            <div className="desc">Split</div>
+          </div>
         </section>
-        </div>
-    )
+      </div>
+    </>
+  );
 }
 export default Home;
