71f8be51e222641ec2d3b1fadba391a7ebee1b2f
Francisco Vasquez <franjavi4111@gmail.com>
2020-09-11 16:31:33 -0500

    likes

--- src/containers/Likes.jsx
+++ src/containers/Likes.jsx
@@ -1,34 +1,63 @@
-import React from 'react';
-import './styles/Likes.css';
+import React from "react";
+import "./styles/Likes.css";
 
 function Likes() {
-    return (
-        <div className="likes">
-            <h1 className="likes-tittle">Conozcamos tus gustos...</h1>
-            <div className="likes__content">
-                <td className="likes__content-column">
-                    <tr>Terror<input id="terror" className="likes__column__checkbox" name="remember" type="checkbox" checked /></tr>
-                    <tr>Romance<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Ciencia ficción<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Dramas<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Animes<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Documentales<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                </td>
-                <td>
-                    <tr>Comedia<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Acción<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Telenovelas<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Misterio<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Fantasía<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                    <tr>Policiales<input className="likes__column__checkbox" name="remember" type="checkbox" defaultChecked /></tr>
-                </td>
-                <button className="likes__button">Registrar</button>
-                <div id="info">
-                    <p id="def">Terror es...</p>
-                </div>
-            </div>
+  return (
+    <div className="likes">
+      <h1 className="likes-tittle">Conozcamos tus gustos...</h1>
+      <form className="likes__form">
+        <div className="likes__form-data">
+          <div className="likes__form-data-group1">
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="comedia"
+                value="terror"
+                type="checkbox"
+              />
+              Terror
+            </label>
+            <br></br>
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="terror"
+                value="comedia"
+                type="checkbox"
+              />
+              Comedia
+            </label>
+            <br></br>
+          </div>
+          <div className="likes__form-data-group2">
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="comedia"
+                value="terror"
+                type="checkbox"
+              />
+              Terror
+            </label>
+            <br></br>
+            <label className="likes__column-label">
+              <input
+                className="likes__column__checkbox"
+                name="terror"
+                value="comedia"
+                type="checkbox"
+              />
+              Comedia
+            </label>
+            <br></br>
+        <button type="submit" className="likes__button">
+          ¡Listo!
+        </button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 export default Likes;
